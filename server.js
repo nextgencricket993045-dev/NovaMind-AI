54ee892b7350c7c6104e9d7b9b6eff2a23737b23
nextgencricket993045-dev <nextgencricket993045@gmail.com>
2026-07-02 22:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,58 @@
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = process.env.GEMINI_API_KEY;
+
+app.post("/chat", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: message
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    const reply =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "Sorry, I couldn't understand.";
+
+    res.json({ reply });
+
+  } catch (err) {
+    res.status(500).json({
+      reply: "Server Error"
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
